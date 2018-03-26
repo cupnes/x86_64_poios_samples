22d7bf91174204a64d8249c49fe4f3785b432bc9
Yuma Ohgami <yuma@ohgami.jp>
2018-03-26 21:20:08 +0900

    A01: refactor kernel loading.

--- A01_poiboot/poiboot.c
+++ A01_poiboot/poiboot.c
@@ -17,13 +17,7 @@ void put_param(unsigned short *name, unsigned long long val);
 void efi_main(void *ImageHandle, struct EFI_SYSTEM_TABLE *SystemTable)
 {
 	unsigned long long status;
-	struct EFI_FILE_PROTOCOL *root;
-	struct EFI_FILE_PROTOCOL *file_conf;
-	struct EFI_FILE_PROTOCOL *file_kernel;
 	struct EFI_FILE_PROTOCOL *file_apps;
-
-	unsigned char *p;
-	unsigned int i;
 	unsigned long long kernel_arg1;
 	unsigned long long kernel_arg2;
 	unsigned long long kernel_arg3;
@@ -33,11 +27,13 @@ void efi_main(void *ImageHandle, struct EFI_SYSTEM_TABLE *SystemTable)
 
 	puts(L"Starting OS5 UEFI bootloader ...\r\n");
 
+	struct EFI_FILE_PROTOCOL *root;
 	status = SFSP->OpenVolume(SFSP, &root);
 	assert(status, L"SFSP->OpenVolume");
 
 
 	/* load config file */
+	struct EFI_FILE_PROTOCOL *file_conf;
 	status = root->Open(
 		root, &file_conf, CONF_FILE_NAME, EFI_FILE_MODE_READ, 0);
 	assert(status, L"Can't open config file.");
@@ -65,38 +61,40 @@ void efi_main(void *ImageHandle, struct EFI_SYSTEM_TABLE *SystemTable)
 
 
 	/* load the kernel */
+	struct EFI_FILE_PROTOCOL *file_kernel;
 	status = root->Open(
 		root, &file_kernel, KERNEL_FILE_NAME, EFI_FILE_MODE_READ, 0);
 	assert(status, L"root->Open(kernel)");
 
 	unsigned long long kernel_size = get_file_size(file_kernel);
-	puts(L"kernel_size=");
-	puth(kernel_size, 16);
-	puts(L"\r\n");
+	put_param(L"kernel_size", kernel_size);
 
 	struct header {
 		void *bss_start;
 		unsigned long long bss_size;
 	} head;
+
 	unsigned long long head_size = sizeof(head);
-	puts(L"load kernel ... ");
+	puts(L"load kernel head ... ");
 	safety_file_read(file_kernel, (void *)&head, head_size);
+	puts(L"done\r\n");
 
 	kernel_size -= sizeof(head);
+	puts(L"load kernel body ... ");
 	safety_file_read(file_kernel, (void *)kernel_start, kernel_size);
 	puts(L"done\r\n");
+	file_kernel->Close(file_kernel);
 
 	ST->BootServices->SetMem(head.bss_start, head.bss_size, 0);
 
-	puts(L"loaded kernel(first 8 bytes): ");
-	p = (unsigned char *)kernel_start;
-	for (i = 0; i < 8; i++) {
-		puth(*p++, 2);
-		putc(L' ');
-	}
+	puts(L"kernel body(first 16 bytes): 0x");
+	put_n_bytes((unsigned char *)kernel_start, 16);
 	puts(L"\r\n");
-
-	file_kernel->Close(file_kernel);
+	puts(L"kernel body(last 16 bytes): 0x");
+	unsigned char *kernel_last =
+		(unsigned char *)(kernel_start + kernel_size - 16);
+	put_n_bytes(kernel_last, 16);
+	puts(L"\r\n");
 
 
 	/* load the applications */
